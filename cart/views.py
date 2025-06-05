ad07d3700cd2dca4d823c11ebc84bbd5582f7f9d
Zahra Shahsafdari <missshahi89@gmail.com>
2025-06-05 17:56:34 +0330

    first commit

---
+++ cart/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+from django.urls import reverse
+from .cart import Cart
+from shop import models
+from . import forms
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(models.Product, id=product_id)
+    form = forms.CartAddProductForm(request.POST)
+    if form.is_valid():
+        form_data = form.cleaned_data
+        cart.add(product=product,
+                 product_count=form_data['product_count'],
+                 update_count=form_data['update'])
+    return redirect(reverse('cart:cart_detail'))
+
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_product_count_form'] = forms.CartAddProductForm(
+            initial={
+                'product_count': item['product_count'],
+                'update': True}
+        )
+    return render(request, 'cart/detail.html', {'cart': cart})
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(models.Product, id=product_id)
+    cart.remove(product)
+    return redirect(reverse('cart:cart_detail'))
+
+
+
---
+++ shop/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from cart.forms import CartAddProductForm
+from . import models
+
+def index(request):
+    product_list = models.Product.objects.all()[:5]
+    return render(request, 'index.html', {'product_list': product_list})
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def product(request, pk):
+    product_detail = get_object_or_404(models.Product, id=pk)
+    cart_add_product_form = CartAddProductForm()
+    return render(request, 'product.html', {'product_detail': product_detail,
+                                            'cart_add_product_form': cart_add_product_form})
+
+def store(request):
+    return render(request, 'store.html')
