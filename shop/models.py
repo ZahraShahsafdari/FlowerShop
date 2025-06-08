ed94b65383e10e67af106744b366e4a5c690a64e
Zahra Shahsafdari <missshahi89@gmail.com>
2025-06-08 17:27:46 +0330

    modified

--- shop/models.py
+++ shop/models.py
@@ -42,6 +42,7 @@ class OrderItem(models.Model):
 class Invoice(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     invoice_date = models.DateTimeField(auto_now_add=True)
+    authority = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return str(self.id)
