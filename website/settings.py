47b1f26e0b888f9224395910460129ffcdb8be55
Zahra Shahsafdari <missshahi89@gmail.com>
2025-07-02 16:13:22 +0330

    updated without secret keys

--- website/settings.py
+++ website/settings.py
@@ -156,11 +156,3 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
-SITE_ID = 1
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'zara171989'
